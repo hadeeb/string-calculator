ba20b11b882bf3ed93748ca34c2eae153d327d57
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2025-06-12 18:38:59 +0530

    implement new delimiter `\n`

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export function add(numbers: string): number {
 	if (cleanedString === "") return 0;
 
 	return cleanedString
-		.split(",")
+		.split(/[,\n]/)
 		.map((num) => Number.parseInt(num.trim()), 10)
 		.reduce((sum, num) => sum + num, 0);
 }

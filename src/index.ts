0bf935af987fa8b0b11e1c3bfc72234a0db4cffd
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2025-06-12 19:13:42 +0530

    add custom delimiter syntax

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
+function getInputAndDelimiter(numbers: string): {
+	input: string;
+	delimiter: string | RegExp;
+} {
+	const delimiterPrefix = "//";
+	if (numbers.startsWith(delimiterPrefix)) {
+		// Split the first line and the rest
+		const [delimiterLine, rest] = numbers.split("\n", 2);
+		const delimiter = delimiterLine.slice(delimiterPrefix.length);
+		// If delimiter is empty or \n, this will be empty
+		if (delimiter === "") throw new TypeError("No/Invalid delimiter");
+		return { input: rest, delimiter };
+	}
+
+	// Return the default delimiter
+	return { input: numbers, delimiter: /[,\n]/ };
+}
+
 export function add(numbers: string): number {
 	const cleanedString = numbers.trim();
 	if (cleanedString === "") return 0;
+	const { input, delimiter } = getInputAndDelimiter(cleanedString);
 
-	return cleanedString
-		.split(/[,\n]/)
+	return input
+		.split(delimiter)
 		.map((num) => Number.parseInt(num.trim()), 10)
 		.reduce((sum, num) => sum + num, 0);
 }

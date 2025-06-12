040240fb883ecb66c5aa7288519de28da2424317
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2025-06-12 19:11:46 +0530

    re-group test cases

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,17 +2,30 @@ import { expect, test, describe } from "vitest";
 import { add } from "./index.js";
 
 describe("Add Function", (t) => {
-	const inputOutputMap = new Map([
-		["", 0],
-		["1", 1],
-		["1,5", 6],
-		["1\n2,3", 6],
-		["//;\n1;2", 3],
-	]);
+	describe("Default delimiter", () => {
+		const inputOutputMap = new Map([
+			["", 0],
+			["1", 1],
+			["1,5", 6],
+			["1\n2,3", 6],
+		]);
 
-	for (const [input, output] of inputOutputMap) {
-		test(`Input String("${input}")`, () => {
-			expect(add(input)).toBe(output);
-		});
-	}
+		for (const [input, output] of inputOutputMap) {
+			test(`Input String("${input}")`, () => {
+				expect(add(input)).toBe(output);
+			});
+		}
+	});
+	describe("Custom delimiter", () => {
+		const inputOutputMap = new Map([
+			["//;\n1;2", 3],
+			["//x\n4x6", 10],
+		]);
+
+		for (const [input, output] of inputOutputMap) {
+			test(`Input String("${input}")`, () => {
+				expect(add(input)).toBe(output);
+			});
+		}
+	});
 });
